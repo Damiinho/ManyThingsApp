00f996101ae27f5ec380bd35de6764f8a311afff
Damian Ludef <damiinho@gmail.com>
2023-04-04 16:29:06 +0200

    fix router basename

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Footer from "./Footer";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_RL}>
         <Nav />
         <Main />
         <Footer />
